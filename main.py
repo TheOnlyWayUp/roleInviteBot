625e81ec537782583b5acc21cf97c7671e384808
TheOnlyWayUp <76237496+TheOnlyWayUp@users.noreply.github.com>
2021-12-07 21:47:11 +0530

    Member Join & Member Leave completed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord, pickledb, asyncio
+from discord import user
 from discord.ext import commands, tasks
 from rich.console import Console
 from rich.theme import Theme
@@ -24,6 +25,13 @@ class invite():
             return False
     def getAll(self):
         return self.db
+    def delete(self, key):
+        try:
+            self.db.pop(key, None)
+            return True
+        except:
+            return False
+
 inviteDict = invite()
 userDict = invite()
 async def updateInvites(guild):
@@ -32,7 +40,7 @@ async def updateInvites(guild):
         inviteDict.set(invite.code, {"uses":invite.uses, "inviter":invite.inviter.id, "channel":invite.channel.id, })
         if userDict.get(str(invite.inviter.id)):
             continue
-        userDict.set(str(invite.inviter.id), {"invitedBy":None, "invited":set(), "verified":0})
+        userDict.set(str(invite.inviter.id), {"invitedBy":None, "invited":{"invited":set(), "verifiedInvites":set()}})
         
 
 @bot.event
@@ -53,25 +61,44 @@ async def on_member_join(member):
     for invite in invites:
         if invite.uses > inviteDict.get(invite.code)["uses"]:
             console.log(f'[green]{member.name}[/] has joined the server with an invite that has been used {invite.uses - inviteDict.get(invite.code)["uses"]} times. And was invited by {invite.inviter.name}.')
+            userDict.set(str(member.id), {"invitedBy":invite.inviter.id, "invited":{"invited":set(), "verifiedInvites":set()}})
             bruh = userDict.get(str(invite.inviter.id))
-            bruh["invited"].update([member.id])
-            userDict.set(str(invite.inviter.id), bruh)
+            bruh["invited"]['invited'].update([member.id])
+            e = userDict.set(str(invite.inviter.id), bruh)
+            print(e)
+            console.log(bruh)
             #console.log("Trying to set invites")
-            userDict.set(str(member.id), {"invitedBy":invite.inviter.id, "invited":set()})
-            console.log(f'[green]{member.name}[/] has been added to the list of users who invited {invite.inviter.name}. {inviteDict.get(str(invite.inviter.id))}')
+            console.log(f'[green]{member.name}[/] has been added to the list of users who invited {invite.inviter.name}. {userDict.get(str(invite.inviter.id))}')
             break
     await updateInvites(member.guild)
 
 @bot.event
-async def on_member_update(before, after):
-    if before.roles != after.roles:
-        if len(before.roles) < len(after.roles):
-            if roleId in [id.id for id in after.roles]:
-                console.log(f'[green]{after.name}[/] has been verified.')
-                userDict.set(str(after.id), {"verified":1})
-            console.log(f'[green]{after.name}[/] has gained a role.')
-            return
-        console.log(f'[green]{after.name}[/] has lost a role.')
+async def on_member_remove(member):
+    bruh = userDict.get(str(member.id))
+    inviter = userDict.get(str(bruh["invitedBy"]))
+    inviter["invited"]["invited"].discard(member.id)
+    inviter['invited']['verifiedInvites'].discard(member.id)
+    userDict.set(str(bruh["invitedBy"]), inviter)
+    userDict.delete(str(member.id))
+# @bot.event
+# async def on_member_update(before, after):
+#     if before.roles != after.roles:
+#         if len(before.roles) < len(after.roles):
+#             if roleId in [id.id for id in after.roles]:
+#                 console.log(f'[green]{after.name}[/] has been verified.')
+#                 toModify = userDict.get(userDict.get(str(after.id))['invitedBy'])
+#                 toModify["invited"]['verifiedInvites'].update([after.id])
+#                 userDict.set(str(after.id), toModify)
+#                 return
+#             console.log("Status of L77 - ")
+#             invitedBy = userDict.get(str(after.id))['invitedBy']
+#             toModify = userDict.get(invitedBy)
+#             toModify["invited"]['verifiedInvites'].remove([after.id])
+#             userDict.set(str(after.id), toModify)
+#             console.log(f'[green]{after.name}[/] has gained a role.')
+#             return
+
+#         console.log(f'[green]{after.name}[/] has lost a role.')
 
 @tasks.loop(seconds=10)
 async def dump():
@@ -79,4 +106,4 @@ async def dump():
     print(userDict.getAll())
 dump.start()
 
-bot.run("token")
+bot.run("ODgzMDQ3MTk1Mjk1NzY0NTIx.YTEPyQ.-6N9aaoW1Qksn07BrLq0g6RNINo")
